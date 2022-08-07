2f64ce9db22d024c5c9b9be2548be04f9ed60545
shiningsunnyday <msun415@stanford.edu>
2022-08-06 20:53:24 -0700

    stratify subsampling

--- datasets/cifar10_subsampled.py
+++ datasets/cifar10_subsampled.py
@@ -1,19 +1,17 @@
 from torchvision.datasets import CIFAR10
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 class CIFAR10Subsampled(CIFAR10):
 
-    def __init__(self, root, train, train_len, seed=0, transform=None, target_transform=None, download=False):
+    def __init__(self, root, train, probe_train_frac, seed=0, transform=None, target_transform=None, download=False):
         super().__init__(
             root=root, train=train, transform=transform, target_transform=target_transform,
             download=download)
         cifar_len = super().__len__()
         if train:
-            assert train_len <= cifar_len
-            rng = np.random.default_rng(seed=seed)
-            self._indices = rng.choice(cifar_len, train_len).astype(np.int32)
-            self.data = np.array(self.data)[self._indices]
-            self.targets = np.array(self.targets)[self._indices]
+            self.data, _, self.targets, _, self._indices, _ = train_test_split(self.data, self.targets, np.arange(cifar_len), 
+            train_size=probe_train_frac, random_state=seed, stratify=self.targets)            
         else:
             self._indices = np.arange(cifar_len)
 

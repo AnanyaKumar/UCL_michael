73b60df61cdfb5fa951905a5b009a75592ad8736
Divyam Madaan <divyam3897@gmail.com>
2022-10-19 13:59:47 -0400

    Update lwf

--- models/lwf.py
+++ models/lwf.py
@@ -24,7 +24,6 @@ class Lwf(ContinualModel):
     NAME = 'lwf'
     COMPATIBILITY = ['class-il', 'task-il']
 
-    def __init__(self, backbone, loss, args, transform):
     def __init__(self, backbone, loss, args, len_train_loader, transform):
         super(Lwf, self).__init__(backbone, loss, args, len_train_loader, transform)
         self.old_net = None
@@ -36,17 +35,16 @@ class Lwf(ContinualModel):
         nc = get_dataset(args).N_TASKS * self.cpt
         self.eye = torch.tril(torch.ones((nc, nc))).bool().to(self.device)
 
-    def begin_task(self, dataset):
+    def begin_task(self, train_loader):
         self.net.eval()
         if self.current_task > 0:
-            # warm-up
-            opt = SGD((self.net.module.backbone.fc.parameters(), lr=self.args.lr)
-            for epoch in range(self.args.n_epochs):
-                for idx, ((inputs, images2, notaug_images), labels) in enumerate(dataset.train_loader):
+            opt = SGD(self.net.backbone.fc.parameters(), lr=self.args.train.lp_lr)
+            for epoch in range(self.args.train.num_lp_epochs):
+                for idx, ((inputs, images2, notaug_images), labels) in enumerate(train_loader):
                     inputs, labels = inputs.to(self.device), labels.to(self.device)
                     opt.zero_grad()
                     with torch.no_grad():
-                        feats = self.net.backbone(inputs, returnt='features')
+                        feats = self.net.backbone(inputs, return_features=True)
                     mask = self.eye[(self.current_task + 1) * self.cpt - 1] ^ self.eye[self.current_task * self.cpt - 1]
                     outputs = self.net.backbone.fc(feats)[:, mask]
                     loss = self.loss(outputs, labels - self.current_task * self.cpt)
@@ -55,29 +53,30 @@ class Lwf(ContinualModel):
 
             logits = []
             with torch.no_grad():
-                for i in range(0, dataset.train_loader.dataset.data.shape[0], self.args.batch_size):
-                    inputs = torch.stack([dataset.train_loader.dataset.__getitem__(j)[0][2]
-                                          for j in range(i, min(i + self.args.batch_size,
-                                                         len(dataset.train_loader.dataset)))])
+                for i in range(0, train_loader.dataset.data.shape[0], self.args.train.batch_size):
+                    inputs = torch.stack([train_loader.dataset.__getitem__(j)[0][0]
+                                          for j in range(i, min(i + self.args.train.batch_size,
+                                                         len(train_loader.dataset)))])
                     log = self.net.backbone(inputs.to(self.device)).cpu()
                     logits.append(log)
-            setattr(dataset.train_loader.dataset, 'logits', torch.cat(logits))
+            setattr(train_loader.dataset, 'logits', torch.cat(logits))
         self.net.train()
 
         self.current_task += 1
 
-    def observe(self, inputs, labels, not_aug_inputs, logits=None):
+    def observe(self, inputs1, labels, inputs2, notaug_inputs, logits=None):
         self.opt.zero_grad()
-        outputs = self.net.backbone(inputs)
+        outputs = self.net.backbone(inputs1.to(self.device))
 
         mask = self.eye[self.current_task * self.cpt - 1]
-        loss = self.loss(outputs[:, mask], labels)
+        loss = self.loss(outputs[:, mask], labels.to(self.device))
         if logits is not None:
             mask = self.eye[(self.current_task - 1) * self.cpt - 1]
-            loss += self.args.alpha * modified_kl_div(smooth(self.soft(logits[:, mask]).to(self.device), 2, 1),
+            loss += self.args.train.alpha * modified_kl_div(smooth(self.soft(logits[:, mask]).to(self.device), 2, 1),
                                                       smooth(self.soft(outputs[:, mask]), 2, 1))
 
         loss.backward()
         self.opt.step()
+        data_dict = {'loss': loss}
 
-        return loss.item()
+        return data_dict
